60669de809058f76072ff32f1eac08abf6b74db6
Filip Dabrowski <filip.dabrowski@protonmail.com>
2024-07-30 20:37:13 +0200

    feat: add gen for llama

--- nvim/lua/fufu/plugins/ai/gen.lua
+++ nvim/lua/fufu/plugins/ai/gen.lua
@@ -2,5 +2,10 @@ return {
 	"David-Kunz/gen.nvim",
 	opts = {
 		model = "llama3.1",
+		display_mode = "split",
 	},
+	config = function(_, opts)
+		require("gen").setup(opts)
+		vim.keymap.set({ "n", "v" }, "<leader>]", ":Gen<CR>")
+	end,
 }
